c9f040475de412dcd67cdae3e9ccf340c36be17e
Mohit <mohitkhatri4272@gmail.com>
2023-04-01 23:08:57 +0530

    conditional rendering

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,7 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
    return (
